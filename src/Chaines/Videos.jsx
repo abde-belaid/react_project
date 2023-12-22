81f91b488b9e4aac447399c772a03e87508d2816
Abderrahim-belaid-123 <AbdeBelaid218@gmail.com>
2023-12-22 11:08:07 +0100

    the first commit

---
+++ src/Chaines/Videos.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react'
+
+const Videos = () => {
+    const [videos,Setvideos]=useState([])
+    useEffect(()=>{
+        
+    },[])
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Videos
